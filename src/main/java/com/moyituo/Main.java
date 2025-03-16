b10fce8b06df7c38fc9c9df60af05a9964eb22fd
wangshuo <3047893225@qq.com>
2025-03-16 23:56:03 +0800

    Revert "third"
    This reverts commit 39f4230d0bc309e5b9631b98e530ae29fd8f41af.

--- src/main/java/com/moyituo/Main.java
+++ src/main/java/com/moyituo/Main.java
@@ -5,6 +5,5 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("first");
         System.out.println("second");
-        System.out.println("third");
     }
 }
